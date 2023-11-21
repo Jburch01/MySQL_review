a0cb50c4d970cf64f9fafa7bff8d5e45e129953e
joshua burch <jburch01@gmail.com>
2023-11-21 11:31:11 -0600

    finished count

---
+++ section_9.sql
@@ -0,0 +1,8 @@
+USE book_shop;
+SELECT * FROM books;
+
+SELECT COUNT(*) AS num_rows FROM books;
+SELECT COUNT(DISTINCT author_fname) AS num_authors FROM books;
+SELECT COUNT(DISTINCT released_year) AS num_years FROM books;
+SELECT COUNT(DISTINCT author_lname) FROM books;
+SELECT COUNT(*) FROM books WHERE title LIKE '%the%';
