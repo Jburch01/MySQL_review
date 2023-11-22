5db0e8fb23f4480bfaf4b4e185a9bb897df31551
Josh Burch <joshuaburch01@gmail.com>
2023-11-22 11:41:20 -0600

    group by

--- section_9.sql
+++ section_9.sql
@@ -7,3 +7,15 @@ SELECT COUNT(DISTINCT released_year) AS num_years FROM books;
 SELECT COUNT(DISTINCT author_lname) FROM books;
 SELECT COUNT(*) FROM books WHERE title LIKE '%the%';
 Select * FROM books;
+
+SELECT author_lname FROM books 
+GROUP BY author_lname;
+
+SELECT author_lname, COUNT(*) as num_books FROM books 
+GROUP BY author_lname
+ORDER BY num_books DESC;
+
+SELECT released_year, COUNT(*) as num_books FROM books
+GROUP BY released_year
+ORDER BY num_books DESC;
+
