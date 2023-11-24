e83da74437121bb03ebce6c0eaaf33a77a85422b
Josh Burch <joshuaburch01@gmail.com>
2023-11-24 14:43:44 -0600

    subqueries

--- section_9.sql
+++ section_9.sql
@@ -24,5 +24,13 @@ SELECT MAX(released_year) AS latest_year FROM books;
 SELECT MAX(pages) FROM books;
 SELECT MIN(author_lname), MAX(author_lname)FROM books;
 
+SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
+SELECT * FROM books 
+WHERE pages = (SELECT MIN(pages) FROM books);
 
+SELECT * FROM books 
+WHERE pages = (SELECT MAX(pages) FROM books);
+
+SELECT title, released_year FROM books
+WHERE released_year = (SELECT MIN(released_year) FROM books);
 
